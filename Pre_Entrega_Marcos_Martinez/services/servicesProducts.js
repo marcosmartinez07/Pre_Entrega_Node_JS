db54091566437bd50e1bf68e5a0c74609b677e65
Violeta Martinez <delfrancochili@gmail.com>
2026-04-27 20:31:05 -0300

    validation

--- Pre_Entrega_Marcos_Martinez/services/servicesProducts.js
+++ Pre_Entrega_Marcos_Martinez/services/servicesProducts.js
@@ -7,7 +7,18 @@ export async function callApi(method, path, body = null) {
   if (body) options.body = JSON.stringify(body);
 
   const response = await fetch(`https://fakestoreapi.com/${path}`, options);
-  if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText}`);
+
+
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
+
   
-  return await response.json();
+  const text = await response.text();
+
+  if (!text || text === 'null') {
+    throw new Error('No content: El item solicitado no existe');
+  }
+
+  return JSON.parse(text);
 }
